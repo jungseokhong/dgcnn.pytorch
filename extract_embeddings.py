46dbb4c85116dbafc9a641ca1784c7dd489020dc
Jungseok <Jungseok@lambda12.localdomain>
2022-08-17 11:33:29 -0400

    freemugspoon case updated

--- extract_embeddings.py
+++ extract_embeddings.py
@@ -92,8 +92,50 @@ def load_color_free_mug():
         if (row_index >= img_size):
             break  
 
+
+def load_color_free_mug_spoon():
+    colors = []
+    labels = []
+    f = open("prepare_data/meta/free_mug_spoon_colors.txt")
+    for line in json.load(f):
+        colors.append(line['color'])
+        labels.append(line['label'])
+    semseg_colors = np.array(colors)
+    semseg_colors = semseg_colors[:, [2, 1, 0]]
+    partseg_labels = np.array(labels)
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    img_size = 1500
+    img = np.zeros((500, img_size, 3), dtype="uint8")
+    cv2.rectangle(img, (0, 0), (img_size, 750), [255, 255, 255], thickness=-1)
+    color_size = 64
+    color_index = 0
+    label_index = 0
+    row_index = 16
+    for _ in range(0, img_size):
+        column_index = 32
+        for _ in range(0, img_size):
+            color = semseg_colors[color_index]
+            label = partseg_labels[label_index]
+            length = len(str(label))
+            cv2.rectangle(img, (column_index, row_index), (column_index + color_size, row_index + color_size),
+                          color=(int(color[0]), int(color[1]), int(color[2])), thickness=-1)
+            img = cv2.putText(img, label, (column_index + int(color_size * 1.15), row_index + int(color_size / 2)),
+                              font,
+                              0.7, (0, 0, 0), 2)
+            column_index = column_index + 200
+            color_index = color_index + 1
+            label_index = label_index + 1
+            if color_index >= 3:
+                cv2.imwrite("prepare_data/meta/free_mug_spoon_colors.png", img, [cv2.IMWRITE_PNG_COMPRESSION, 0])
+                return np.array(colors)
+            elif (column_index >= 1280):
+                break
+        row_index = row_index + int(color_size * 1.3)
+        if (row_index >= img_size):
+            break  
+
 def calculate_sem_IoU(pred_np, seg_np, visual=False):
-    num_classes = 2
+    num_classes = 3
     I_all = np.zeros(num_classes)
     U_all = np.zeros(num_classes)
     for sem_idx in range(seg_np.shape[0]):
@@ -120,7 +162,7 @@ def visualization(data, seg, pred):
     ## data shape (1,N,6), seg (1,N), pred (1,N)
     data = data[0] # shape (N,6)
     # semseg_colors = load_color_benchmark_seg()
-    semseg_colors = load_color_free_mug()
+    semseg_colors = load_color_free_mug_spoon()
 
 
     # print("color")
@@ -140,8 +182,8 @@ def visualization(data, seg, pred):
     print(mIoU)
     mIoU = str(round(mIoU, 4))
 
-    filepath = 'testdata'+'/'+'test2'+'_pred_'+mIoU+'_'+'.ply'
-    filepath_gt = 'testdata'+'/'+'free_mug'+'_gt.ply'
+    filepath = 'testdata'+'/'+'free_mug_spoon_partial_1'+'_pred_'+mIoU+'_'+'.ply'
+    filepath_gt = 'testdata'+'/'+'free_mug_spoon_partial_1'+'_gt.ply'
     xyzRGB = [(xyzRGB[i, 0], xyzRGB[i, 1], xyzRGB[i, 2], xyzRGB[i, 3], xyzRGB[i, 4], xyzRGB[i, 5]) for i in range(xyzRGB.shape[0])]
     xyzRGB_gt = [(xyzRGB_gt[i, 0], xyzRGB_gt[i, 1], xyzRGB_gt[i, 2], xyzRGB_gt[i, 3], xyzRGB_gt[i, 4], xyzRGB_gt[i, 5]) for i in range(xyzRGB_gt.shape[0])]
     vertex = PlyElement.describe(np.array(xyzRGB, dtype=[('x', 'f4'), ('y', 'f4'), ('z', 'f4'), ('red', 'u1'), ('green', 'u1'), ('blue', 'u1')]), 'vertex')
@@ -172,7 +214,7 @@ def extractor(data):
     args = parser.parse_args()
     ## model weights, can be hardcoded.
     # args.model_root = 'outputs/benchmark_6d_1/models/'
-    args.model_root = 'outputs/free_mug_am_cos/models/'
+    args.model_root = 'outputs/free_mug_spoon/models/'
 
 
     ## Model related paramters
@@ -238,7 +280,7 @@ def extractor(data):
     tanh_embeddings = tanh_embeddings.detach().cpu().numpy()
     softmax_embeddings = softmax_embeddings.detach().cpu().numpy()
     
-    f = h5py.File('test_2_embeddings_am_cos.h5', 'w')
+    f = h5py.File('part_fms005_0_embeddings_free_mug_spoon_partial.h5', 'w')
     f.create_dataset('embeddings', data = seg_pred_np)
     f.create_dataset('sigmoid_embeddings', data = sigmoid_embeddings)
     f.create_dataset('relu_embeddings', data = relu_embeddings)
@@ -272,14 +314,49 @@ if __name__ == "__main__":
     # print(visualization(data, seg, pred_np))
 
 
+    # ## test case 2
+    # f = h5py.File('data/freemugspoon_data_test/fms_data_019.h5', 'r')
+    # xyzRGB = f['data'][0]
+    # label = f['label'][0]
+    # data = xyzRGB[::].reshape(1,-1,3)
+    # seg = label[::].reshape(1,-1)
+
+    # idx_mug = [i for i in range(0,200)]
+    # idx_spoon = [i for i in range(1000,1200)]
+    # idx_free = [i for i in range(1300,1500)]
+    # idx = idx_mug+idx_spoon+idx_free
+    # partial_data = data[:,idx,:]
+    # partial_seg = seg[:,idx]
+
+    # print("data shape, ", data.shape, " seg shape", seg.shape)
+    # print("partial data shape, ", partial_data.shape, " partial seg shape", partial_seg.shape)
+
+    # # seg_pred_np, pred_np = extractor(data)
+    # # print(pred_np.shape, f['label'][::].reshape(1,-1).shape)
+    # # print(calculate_sem_IoU(pred_np, f['label'][0].reshape(1,-1)))
+    # # print(visualization(data, seg, pred_np))
+
+    # partial_seg_pred_np, partial_pred_np = extractor(partial_data)
+    # print(partial_pred_np.shape, partial_seg.shape)
+    # print(calculate_sem_IoU(partial_pred_np, f['label'][0][idx].reshape(1,-1)))
+    # print(visualization(partial_data, partial_seg, partial_pred_np))
+
     ## test case 2
-    f = h5py.File('data/xyz_data_test/data_021.h5', 'r')
-    xyzRGB = f['data'][0]
-    label = f['label'][0]
+    f = h5py.File('data/freemugspoon_part_data/fms_data_004.h5', 'r')
+    xyzRGB = f['data'][2]
+    label = f['label'][2]
     data = xyzRGB[::].reshape(1,-1,3)
     seg = label[::].reshape(1,-1)
+
+
     print("data shape, ", data.shape, " seg shape", seg.shape)
+
     seg_pred_np, pred_np = extractor(data)
     print(pred_np.shape, f['label'][::].reshape(1,-1).shape)
     print(calculate_sem_IoU(pred_np, f['label'][0].reshape(1,-1)))
     print(visualization(data, seg, pred_np))
+
+    # partial_seg_pred_np, partial_pred_np = extractor(partial_data)
+    # print(partial_pred_np.shape, partial_seg.shape)
+    # print(calculate_sem_IoU(partial_pred_np, f['label'][0][idx].reshape(1,-1)))
+    # print(visualization(partial_data, partial_seg, partial_pred_np))
